ffdff8a7d2432511c91da5ecbf352f95723c2dfa
Toomore Chiang <toomore0929@gmail.com>
2010-08-31 21:51:37 +0800

    Add except KeyboardInterrupt.

--- htls_cmd.py
+++ htls_cmd.py
@@ -3,22 +3,19 @@
 import htls
 from datetime import datetime
 while True:
-  print '結束請輸入 stop\n'
+  print '結束請按 Ctrl + c\n'
   try:
     name = raw_input('請輸入姓名：')
     age = raw_input('請輸入年齡：')
     year = raw_input('請輸入年度（民國）：')
 
-    if 'stop' in [name, age, year]:
-      break
+    if len(year):
+      year = int(year)
     else:
-      if len(year):
-        year = int(year)
-      else:
-        year = datetime.today().year - 1911
-      print '=' * 20
-      htls.htls().all(name, age, year)
-      print '=' * 20
-  except:
-    pass
+      year = datetime.today().year - 1911
+    print '=' * 20
+    htls.htls().all(name, age, year)
+    print '=' * 20
+  except KeyboardInterrupt:
+    break
 print '\nbye ...'
